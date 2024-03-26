1b24e4a4e9e59b26e9946a7752e375eff6681192
martinademarco <marti.dema@gmail.com>
2024-03-26 16:29:15 +0100

    new_token_marti

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ app = Flask(appname)
 config = configparser.ConfigParser()
 config.read('config.ini')
 client = influxdb_client.InfluxDBClient(url=config.get("InfluxDBClient","Url"),
-   token=config.get("InfluxDBClient","TokenMarti"),
+   token=config.get("InfluxDBClient","TokenMarti2"),
    org=config.get("InfluxDBClient","Org"))
 
 SWAGGER_URL = '/api/docs'  # URL for exposing Swagger UI (without trailing '/')
